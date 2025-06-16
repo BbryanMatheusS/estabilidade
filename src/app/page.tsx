97b1509f9ef89b1de80d17e545997311a1c40c8a
Bryan Matheus <bryan.profissionalll@gmail.com>
2025-06-16 01:07:54 -0300

    corrigindo a importaçao dos serviços

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 import ContactSection from "@/components/Contact";
 import NossaAtuacao from "@/components/NossaAtuacao";
-import Services from "@/components/services";
+import Services from "@/components/Services";
 import SobrePage from "@/components/Sobre";
 import { TeamCarousel } from "@/components/TeamCarousel";
 import { VideoCarousel } from "@/components/VideoCarousel";
